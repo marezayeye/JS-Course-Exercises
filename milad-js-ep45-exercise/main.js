15dc8a46c7cdd19259d2f61417aa128a50af2a94
marezayeye <marezayeye@gmail.com>
2023-06-12 11:30:06 +0330

    added a shorter way for milad-js-ep45-exercise

--- milad-js-ep45-exercise/main.js
+++ milad-js-ep45-exercise/main.js
@@ -9,23 +9,45 @@ const userData = [
 ];
 
 //creating  a list element and append it to div container
+// const listElement = document.createElement("ul");
+// listElement.setAttribute("class", "list");
+
+//adding list items to list element using a for loop
+// for (let i = 0 ; i < userData.length ; i++){
+//     listElement.appendChild(document.createElement("li"));
+// };
+
+//setting class attribute to list items
+// for(let i = 0 ; i < userData.length ; i++){
+//     listElement.children[i].setAttribute("class", "listItem");
+//     listElement.children[i].appendChild(document.createElement("p"));
+//     listElement.children[i].children[0].innerText = `the age of ${userData[i].name} is ${userData[i].age}`;
+    
+// };
+
+//and finally adding unOrderedList to div container.
+// const container = document.querySelector(".container");
+// container.appendChild(listElement);
+
+
+
+/////////////////////////////////////////////
+//method 2, shorter way
+////////////////////////////////////////////
+
+
+//creating  an unOrderedlist
 const listElement = document.createElement("ul");
 listElement.setAttribute("class", "list");
 
-//adding list items to list element using a for loop
-for (let i = 0 ; i < userData.length ; i++){
-    listElement.appendChild(document.createElement("li"));
-};
+//a loop to grab each object from userData, create a list item for it and generate the string for each item based on the values in object.
+userData.forEach(item => {
+    const li = document.createElement("li");
+    listElement.appendChild(li);
+    li.innerText = `the age of ${item.name} is ${item.age}`;
+});
 
-//setting class attribute to list items
-for(let i = 0 ; i < userData.length ; i++){
-    listElement.children[i].setAttribute("class", "listItem");
-    listElement.children[i].appendChild(document.createElement("p"));
-    listElement.children[i].children[0].innerText = `the age of ${userData[i].name} is ${userData[i].age}`;
-    
-};
-
-//and finally adding unOrderedList to div container
+//and finally adding ul to the div container.
 const container = document.querySelector(".container");
 container.appendChild(listElement);
 
